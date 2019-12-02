70ae762e7ba43e7e78ecb7bb1814f4fd9263d82e
anki94-cloud <58022455+anki94-cloud@users.noreply.github.com>
2019-12-02 23:22:40 +0530

    Add files via upload

---
+++ fininfocom.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 02, 2019 at 06:49 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fininfocom`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`product_id`, `name`, `price`, `image`) VALUES
+(1, 'GULAB JAMUN', 4.95, 'https://www.eposhybrid.uk/upload/menus/undefined1253667871.jpeg'),
+(2, 'WARM CHOCOLATE PUDDING', 4.75, 'https://www.eposhybrid.uk/upload/menus/undefined1023229350.jpeg'),
+(3, 'SELECTION OF ICE CREAMS', 3.95, 'https://www.eposhybrid.uk/upload/menus/undefined1426751147.jpeg'),
+(4, 'KULFI 2 scoops', 4.25, 'https://www.eposhybrid.uk/upload/menus/undefined296947488.jpeg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
